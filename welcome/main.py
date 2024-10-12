accaae7ab73f5c237144dd966b2ebeba83f664a3
ds-wook <leewook94@gmail.com>
2024-10-12 23:05:51 +0900

    feat: add code

--- welcome/main.py
+++ welcome/main.py
@@ -3,6 +3,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", summary="Welcome to the API", tags=["Simple"])
 async def read_root():
+    """
+    This is the root path of the API
+    ***args: None
+    ***returns: {"message": "Hello World"}
+    """
     return {"message": "Hello World"}
